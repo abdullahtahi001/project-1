7d1b6ba3f5761c1b8d3d87a644cc65c5fbdc306b
abdullahtahi001 <ouronlineserver@gmail.com>
2024-09-25 21:42:48 +0500

    add.index.php

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 1</title>
+    <?php include_once('includes/site-master.php') ?>
+   
+</head>
+<?php include_once('includes/header.php') ?>
+<!-- =========================banner=================== -->
+<body>
+    <main>
+    <!-- hero section -->
+    <section class="hero-section"></section>
+    <!-- about-section -->
+    <section class="about-section">
+        <div class="container">
+            <div class="flex">
+                <div class="left">
+                    <div class="flex">
+                        <div class="imeges">
+                            <img src="imges/image-1.png" alt="">
+                        </div>
+                        <div class="imeges" id="line">
+                            <img src="imges/image-2.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="content">
+                        <h1>
+                            Step into the World of Tailored Perfection at Straus Tailor Shop
+                        </h1>
+                        <p>At Straus Tailor Shop, perfection is not just a goal—it’s a standard. Our passion for
+                            tailoring goes beyond mere craftsmanship; it’s an art form where every stitch tells a story
+                            of dedication, precision, and style. We invite you to experience
+                            the unparalleled quality of bespoke clothing that is meticulously crafted to reflect your
+                            individuality.</p>
+                        <div class="btn">
+                            <a href="">contect us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- servicees -->
+    <section class="services">
+        <div class="container">
+            <div class="flex">
+                <div class="left">
+                    <h1>Dedicated to Perfection and Elegance</h1>
+                    <p>Our goal at Straus Tailor Shop is to deliver exceptional alterations that enhance the beauty and
+                        fit of every garment, ensuring our clients look and feel their best on their most important
+                        occasions. We are committed to c
+                        ombining our extensive experience with meticulous craftsmanship to exceed your expectations
+                        every time.</p>
+                    <div class="arrow">
+                        <div class="fghl">Delivering precise alterations and customizations.</div>
+                        <div class="fghl"> Specializing in wedding, pageant, and prom attire.</div>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="content">
+                        <div class="imgage">
+                            <img src="imges/br1.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- flex-image   -->
+    <section class="flex-image">
+        <div class="container">
+            <div class="content">
+                <h1>Portfolio</h1>
+                <h2>Outstanding Craftsmanship</h2>
+            </div>
+            <div class="flex">
+                <div class="imge">
+                    <img src="imges/Group 1000003770.png" alt="">
+                </div>
+                <div class="imge">
+                    <img src="imges/Group 1000003769.png" alt="">
+                </div>
+                <div class="imge">
+                    <img src="imges/Group 1000003768.png" alt="">
+                </div>
+                <div class="imge">
+                    <img src="imges/Group 1000003771.png" alt="">
+                </div>
+            </div>
+            <div class="flex">
+                <div class="btn">
+                    <a href="">contect us</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- testimonials -->
+    <section class="testimonial">
+        <div class="container">
+            <div class="content">
+                <h1>Words of Our Clients</h1>
+            </div>
+            <div class="testi-carousel owl-carousel">
+               <div class="item">
+                <div class="contain">
+                    <div class="pera">
+                        <p>Straus Tailor Shop transformed my wedding dress into
+                            something truly special. The attention to detail and
+                            craftsmanship were impeccable. I felt like a princess
+                            on my big day!</p>
+                    </div>
+                    <div class="logo-nav">
+                        — Jessica L., Bride
+                    </div>
+                </div>
+               </div>
+               <div class="item">
+                <div class="contain">
+                    <div class="pera">
+                        <p>Straus Tailor Shop transformed my wedding dress into
+                            something truly special. The attention to detail and
+                            craftsmanship were impeccable. I felt like a princess
+                            on my big day!</p>
+                    </div>
+                    <div class="logo-nav">
+                        — Jessica L., Bride
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+    </section>
+                             <!-- our services -->
+    <section class="OurServices">
+        <div class="container">
+            <div class="content">
+                <h1>Our Services</h1>
+                <h2>What We Offer</h2>
+            </div>
+            <div class="flex">
+                <div class="imges">
+                    <img src="imges/carolyn--mfvo192162.png" alt="">
+                </div>
+                <div class="imges">
+                    <img src="imges/o8QreLLHtEA6CtG8geykASAgIf1YE3WvPOQHbC~tplv-tej9nj120t-origin.png" alt="">
+                </div>
+                <div class="imges">
+                    <img src="imges/portia-and-scarlett-ps22349-sequin-one-shoulder-gown-special-occasion-dress-18-emerald-29220189405267_540x.png" alt="">
+                </div>
+                <div class="imges">
+                    <img src="imges/news-2.png" alt="">
+                </div>
+            </div>
+            <div class="btn">
+                <a href="">contect here</a>
+            </div>
+        </div>
+    </section>
+                                     <!-- .banner2 -->
+    <section class="banner2">
+        <div class="container">
+            <div class="flex">
+            <div class="left">
+               <img src="imges/Group 1000003758.png" alt="">
+            </div>
+            <div class="right">
+                    <div class="content">
+                        <h1>Frequently Asked Questions</h1>
+                    </div>
+            </div>
+        </div>
+        </div>
+    </section>                                  
+</main>
+<?php include_once('includes/footer.php') ?>
+</body>
+</html>
